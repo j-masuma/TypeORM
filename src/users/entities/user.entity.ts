d401db7f2a084a4ad52a45bb942487247049239c
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-07-31 11:15:30 +0500

    Some changes

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number; //auto incrementing int
+
+  @Column()
+  name: string;
+
+  @Column()
+  other: string;
+}
